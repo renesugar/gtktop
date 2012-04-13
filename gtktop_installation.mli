be83f3d462b44762c8eaf3462c869b59ecaad6d4
Zoggy <zoggy@bat8.org>
2012-04-13 15:44:57 +0200

    headers

--- gtktop_installation.mli
+++ gtktop_installation.mli
@@ -1,3 +1,27 @@
+(*********************************************************************************)
+(*                Gtktop                                                         *)
+(*                                                                               *)
+(*    Copyright (C) 2005-2012 Institut National de Recherche en Informatique     *)
+(*    et en Automatique. All rights reserved.                                    *)
+(*                                                                               *)
+(*    This program is free software; you can redistribute it and/or modify       *)
+(*    it under the terms of the GNU Lesser General Public License version        *)
+(*    3 as published by the Free Software Foundation.                            *)
+(*                                                                               *)
+(*    This program is distributed in the hope that it will be useful,            *)
+(*    but WITHOUT ANY WARRANTY; without even the implied warranty of             *)
+(*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *)
+(*    GNU General Public License for more details.                               *)
+(*                                                                               *)
+(*    You should have received a copy of the GNU General Public License          *)
+(*    along with this program; if not, write to the Free Software                *)
+(*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA                   *)
+(*    02111-1307  USA                                                            *)
+(*                                                                               *)
+(*    Contact: Maxence.Guesdon@inria.fr                                          *)
+(*                                                                               *)
+(*********************************************************************************)
+
 val glade_dir : string
 val software_version : string
 val glade_file : string
